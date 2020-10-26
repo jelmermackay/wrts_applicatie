5b03bb9ac8624bdb2b5b94c6b4dfd45d2d485209
jelmermackay <54801634+jelmermackay@users.noreply.github.com>
2020-10-26 09:28:43 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,6 +1,5 @@
 <?php
-// $conn = new mysqli('localhost', 'student4a8_534388', 'Q96fqy', 'student4a8_534388');
-$conn = new mysqli('localhost', 'root', '', 'student4a8_534388');
+$conn = new mysqli('localhost', 'root', '', 'wrts');
 global $conn;
 session_start();
 
@@ -8,28 +7,4 @@ if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
     exit();
 }
-
-// // aanmaken voor de lijsten
-// if (isset($_POST["sendList"])) {
-//     $nameList = $_POST["listName"];
-//     $user_id = $_SESSION["user_id"];
-    
-//     $stmt = $conn->prepare("INSERT INTO list (user_id, list_name) VALUES (?, ?)");
-//     $stmt->bind_param("is", $user_id, $nameList);
-//     $stmt->execute();
-//     $stmt->close();
-// }
-
-// // voor het aanmaken van de woorden
-// if (isset($_POST["saveWord"])) {
-//     $dutchWord = $_POST["dutch"];
-//     $englishWord = $_POST["english"];
-//     $list_id = $_POST["list_id"];
-
-//     $stmt = $conn->prepare("INSERT INTO word (dutch, english, list_id) VALUES (?, ?, ?)");
-//     $stmt->bind_param("ssi", $dutchWord, $englishWord, $list_id);
-//     $stmt->execute();
-//     $stmt->close();
-// }
-
 ?>
